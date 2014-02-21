6d3a01082da9d56e4e7b7d242dd465461f82f560
Akshat Pradhan <akshat.ip@gmail.com>
2014-02-21 17:13:18 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComplianceChimp::Application.config.secret_token = '447ea4c234663ad0009b4cb9f864d3c35e55795826e7b507444d01ee46289ce707b6af383bf8954a0cd8f9f38b9466e099f0ef2de7dc548d1736f7a3a5cebecb'
