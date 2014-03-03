51a81b564dd1c32affa86773957fab2919805f1b
Akshat Pradhan <akshat.ip@gmail.com>
2014-03-03 15:55:17 -0500

    user views standards after logging in
    * clean up some whitespace and rails 3.2 syntax
    * rename requirement field to name field for Standard in order to stick
    with Rails conventions
    * add separate tests to show PCI standards for guest and logged in users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
     if user.email.blank?
       redirect_to edit_user_path(user), :alert => "Please enter your email address."
     else
-      redirect_to root_url, :notice => 'Signed in!'
+      redirect_to root_path, notice: 'Signed in!'
     end
 
   end
