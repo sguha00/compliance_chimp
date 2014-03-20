debd1ed42b63724141e6608e08d2919cdb78d1e9
Andrea D'Amico <andrea.damico@gmail.com>
2014-03-20 09:48:02 +0100

    restructuring manage requirements spec

--- spec/features/manage_requirements_spec.rb
+++ spec/features/manage_requirements_spec.rb
@@ -20,36 +20,41 @@ feature 'Manage proofs' do
     visit signin_path
   end
 
-  # Then user expects to see requirements and proofs
-  scenario 'user views all requirements and proofs' do
-    expect(page).to have_css '[data-role="current-user"]', text: user.name
-    expect(page).to have_xpath "//img[@src='#{proof.image_url}']"
-    expect(page).to have_css '[data-role="requirement"]', text: '1.1.1'
-    expect(page).to have_css '[data-role="requirement"]', text: '1.1.2'
-  end
-
-  context "for requirements with no proof" do
-    # When user adds a proof
-    # Then user expects to see the proof image
-    scenario 'user adds a new proof image' do
-      click_link 'Attach proof for 1.1.2'
-      fill_in 'proof_image_url', with: 'http://www.imgur.com/1.1.2.png'
-      click_button 'Submit'
-      expect(page).to have_xpath '//img[@src="http://www.imgur.com/1.1.2.png"]'
+  describe "user browsing requirements and proofs" do
+    # Then user expects to see requirements and proofs
+    scenario 'user views all requirements and proofs' do
+      expect(page).to have_css '[data-role="current-user"]', text: user.name
+      expect(page).to have_xpath "//img[@src='#{proof.image_url}']"
+      expect(page).to have_css '[data-role="requirement"]', text: '1.1.1'
+      expect(page).to have_css '[data-role="requirement"]', text: '1.1.2'
     end
   end
 
-  context "for requirements with existing proof" do
-    # When user updates a proof
-    # Then user expects to see the updated proof
-    scenario "user updates proof" do
-      click_link 'Update proof for 1.1.1'
-      fill_in 'proof_image_url', with: 'http://www.imgur.com/1.1.1-updated_today.png'
-      click_button 'Submit'
-      expect(page).to have_xpath '//img[@src="http://www.imgur.com/1.1.1-updated_today.png"]'
+  describe "user wishing to fulfill requirements" do
+    context "for requirements with no proof" do
+      # When user adds a proof
+      # Then user expects to see the proof image
+      scenario 'adds a new proof image' do
+        click_link 'Attach proof for 1.1.2'
+        fill_in 'proof_image_url', with: 'http://www.imgur.com/1.1.2.png'
+        click_button 'Submit'
+        expect(page).to have_xpath '//img[@src="http://www.imgur.com/1.1.2.png"]'
+      end
     end
-    scenario "user expects to not see an 'Attach proof' link" do
-      expect(page).not_to have_link "Attach proof for 1.1.1"
+
+    context "for requirements with existing proof" do
+      # When user updates a proof
+      # Then user expects to see the updated proof
+      scenario "updates proof" do
+        click_link 'Update proof for 1.1.1'
+        fill_in 'proof_image_url', with: 'http://www.imgur.com/1.1.1-updated_today.png'
+        click_button 'Submit'
+        expect(page).to have_xpath '//img[@src="http://www.imgur.com/1.1.1-updated_today.png"]'
+      end
+      # Then user should not be able to add a new proof
+      scenario "should not see an 'Attach proof' link" do
+        expect(page).not_to have_link "Attach proof for 1.1.1"
+      end
     end
   end
 end
