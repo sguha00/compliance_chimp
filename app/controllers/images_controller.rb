b9699b7ceee8446dcc6584f10223686c95826e1d
Andrea D'Amico <andrea.damico@gmail.com>
2014-03-27 21:20:44 +0100

    destroy screenshot when user clicks cancel * refactor deleting old images when updating proof

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,14 @@
+class ImagesController < ApplicationController
+  def destroy
+    image_url = params[:image_url]
+    complete_delete_url = image_url+"?key=#{ENV["FILEPICKER_API_KEY"]}"
+    HTTParty.delete(complete_delete_url)
+    redirect_to requirements_url, notice: message
+  end
+
+  private
+
+  def message
+    @message ||= params[:message].present? ? params[:message] : "Unsaved image has been successfully removed from filepicker service."
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ComplianceChimp::Application.routes.draw do
   root :to => "home#index"
+  get "images/destroy", to: "images#destroy", as: "destroy_image"
   resources :requirements, only: :index do
     resources :proofs, only: [:new, :edit, :create, :update], shallow: true
   end
