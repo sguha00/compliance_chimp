98068002189f9a4e917fd413296d326d0ebbe979
Andrea D'Amico <andrea.damico@gmail.com>
2014-03-29 15:01:31 +0100

    add note, image, link to pdf

--- app/pdfs/requirements_pdf.rb
+++ app/pdfs/requirements_pdf.rb
@@ -1,7 +1,7 @@
 require "open-uri"
 class RequirementsPdf < Prawn::Document
   def initialize requirements, view
-    super top_margin: 20
+    super top_margin: 20, page_layout: :landscape
     @requirements = requirements
     @view = view
     header
@@ -15,14 +15,27 @@ class RequirementsPdf < Prawn::Document
 
   def requirements_table
     data = [].tap do |data|
-      data << ["#", "Description", "Note", "Proof"]
+      data << ["#", "Description", "Note", {content: "Proof", colspan: 2}]
     end
     @requirements.each do |req|
-      data << [req.name, req.description, "", (req.proof_for(@current_user).nil? ? "" : {image: open(@view.src_for_img_of_proof_for(req))})]
+      data << [req.name, req.description, proof_of(req).try(:note), image_for_proof(req), link_for_image_of_proof_of(req)]
     end
 
     font_size 10 do
-      table data, header: true, column_widths: {0 => 40, 2 => 40, 3 => 40}
+      table data, header: true, column_widths: {0 => 40, 2 => 100, 3 => 150, 4 => 60}, cell_style: { inline_format: true }
     end
+  end
+
+  def proof_of(requirement)
+    requirement.proof_for(@view.current_user)
+  end
+
+  def link_for_image_of_proof_of(requirement)
+    link = proof_of(requirement).try(:filepicker_url)
+    link ? "<link href='#{link}'><u><color rgb='0000FF'>LINK</color></u></link>" : ""
+  end
+
+  def image_for_proof(requirement)
+    proof_of(requirement) ? {image: open(proof_of(requirement).try(:filepicker_url)), scale: 0.1} : ""
   end
 end
