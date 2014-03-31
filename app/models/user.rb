352743963d5160bb8300e13f5948f8713e5a2a65
Andrea D'Amico <andrea.damico@gmail.com>
2014-03-31 19:41:38 +0200

    ensure user email is unique and correctly formatted * validate email on update only * render form errors for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User
   # run 'rake db:mongoid:create_indexes' to create indexes
   index({ email: 1 }, { unique: true, background: true })
 
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, on: :update
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
