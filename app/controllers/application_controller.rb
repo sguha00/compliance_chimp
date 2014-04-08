c195ceeff54a325521bf1437cd7e150e18584df2
Akshat Pradhan <akshat.ip@gmail.com>
2014-04-08 11:20:28 -0400

    user wants to see public pages (e.g. about, faq, find qsa)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  check_authorization
+  check_authorization unless: :static_page?
 
   helper_method :current_user
   helper_method :user_signed_in?
 
   private
+
+  def static_page?
+    self.class == HighVoltage::PagesController
+  end
+
   def current_user
     begin
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- spec/features/view_homepage_spec.rb
+++ spec/features/view_homepage_spec.rb
@@ -2,9 +2,24 @@ require 'spec_helper'
 
 # Given guest is on the homepage
 # Then guest should see PCI Requirements
-feature 'View the homepage' do
-  scenario 'guest expects to see marketing copy' do
+feature 'Guest views ' do
+  scenario 'homepage and expects to see marketing copy' do
     visit root_path
     expect(page).to have_content 'Compliance chimp'
+  end
+
+  scenario 'about page expects to see about copy' do
+    visit page_path('about')
+    expect(page).to have_content 'Akshat Pradhan'
+  end
+
+  scenario 'faq page expects to see faq copy' do
+    visit page_path('faq')
+    expect(page).to have_content 'FAQ'
+  end
+
+  scenario 'find a qsa page expects to see find a qsa copy' do
+    visit page_path('find-a-qsa')
+    expect(page).to have_content 'Find a QSA!'
   end
 end
